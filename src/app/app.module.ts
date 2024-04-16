f42c5f0c53d11479b7ed5665419c937bc0fc3b02
Aditya Silwal <silwala@sheridancollege.ca>
2024-04-15 21:45:12 -0400

    finish coding input page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { OutputComponent } from './output/output.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatCard, MatCardTitle} from "@angular/material/card";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {MatButton} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -14,9 +22,22 @@ import { OutputComponent } from './output/output.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatCard,
+    MatFormField,
+    MatCardTitle,
+    MatInput,
+    MatLabel,
+    MatRadioGroup,
+    MatRadioButton,
+    MatError,
+    MatCheckbox,
+    MatButton
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Tip} from "../tip";
+import {Router} from "@angular/router";
+import {TipDataService} from "../tip-data.service";
 
 @Component({
   selector: 'app-input',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './input.component.css'
 })
 export class InputComponent {
+  public title: string = "Tip calculator";
+  public form: FormGroup;
+  public tip: Tip;
+  private router: Router;
 
+  public constructor(tipService: TipDataService, router: Router) {
+    this.tip = tipService.tip;
+    this.router = router;
+
+    this.form = new FormGroup({
+      costOfService: new FormControl('', Validators.required),
+      tipPercent: new FormControl('', Validators.required),
+      roundUp: new FormControl(false)
+    });
+  }
+
+  public send(data: Tip) {
+    this.tip.costOfService = data.costOfService;
+    this.tip.tipPercent = data.tipPercent;
+    this.tip.roundUp = data.roundUp;
+    this.router.navigate(['output']).then(() => {
+      console.log('showing the output data');
+      console.log(data);
+    });
+  }
 }
